e06997f6ba19ef090859013e669998e88da958fd
GouTouLi-Yu <983451098@qq.com>
2025-05-06 02:30:58 +0800

    项目工程

---
+++ assets/scripts/Main.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component } from "cc";
+const { ccclass, property } = _decorator;
+
+@ccclass("Main")
+export class Main extends Component {
+    protected onLoad(): void {}
+
+    start() {
+        let a = 3;
+        let b = 5;
+    }
+
+    update(deltaTime: number) {}
+}
