7143f3dbb68bb78c9cf361f26f9f2c3e3953d181
GouTouLi-Yu <983451098@qq.com>
2025-05-08 01:31:38 +0800

    文件

--- assets/scripts/project/manager/ResManager.ts
+++ assets/scripts/project/manager/ResManager.ts
@@ -1,1 +1,18 @@
-export class ResManager {}
+import { instantiate, Node, Prefab, resources } from "cc";
+
+export class ResManager {
+    static loadPrefab(path: string): Promise<Node> {
+        return new Promise<Node>((resolve, reject) => {
+            resources.load(path, Prefab, (err: Error, prefab: Prefab) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    let node = instantiate(prefab);
+                    if (node) {
+                        resolve(node);
+                    }
+                }
+            });
+        });
+    }
+}
