9c4d29e2b4a411ed8032105f7ebf0db0e37ee7f1
GouTouLi-Yu <983451098@qq.com>
2025-05-18 23:02:05 +0800

    传送界面

--- assets/scripts/core/view/transmit/TransmitMediator.ts
+++ assets/scripts/core/view/transmit/TransmitMediator.ts
@@ -1,24 +1,11 @@
 import { _decorator } from "cc";
 import { ClassConfig } from "../../../project/config/ClassConfig";
-import { PCEventType } from "../../../project/event/EventType";
 import { AreaMediator } from "../AreaMediator";
 const { ccclass, property } = _decorator;
 
 export class TransmitMediator extends AreaMediator {
     static fullPath: string = "prefab/transmit/";
-    initialize() {
-        this.mapEventListener(
-            PCEventType.EVT_MAIN_MENU_START_GAME,
-            this,
-            () => {
-                console.log("事件触发成功");
-            }
-        );
-    }
-
-    xiake() {
-        console.log("下课了");
-    }
+    initialize() {}
 
     onRegister() {}
 
